892044305b4db52232ab6751856987d5c8881818
donbaba12 <aly042@uottawa.ca>
2024-11-10 14:03:33 -0500

    lab8

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -1,8 +1,32 @@
 package seg3x02.employeeGql.resolvers
 
 import org.springframework.stereotype.Controller
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.stereotype.Component
+import seg3x02.employeeGql.entity.Employee
+import seg3x02.employeeGql.repository.EmployeesRepository
 
-@Controller
-class EmployeesResolver {
+@Component
+class EmployeeResolver(private val employeeRepository: EmployeeRepository) {
 
+    @QueryMapping
+    fun employees(): List<Employee> = employeeRepository.findAll()
+
+    @QueryMapping
+    fun employee(id: String): Employee? = employeeRepository.findById(id)
+
+    @MutationMapping
+    fun addEmployee(name: String, dateOfBirth: String, city: String, salary: Float, gender: String, email: String): Employee {
+        val newEmployee = Employee(
+            id = (employeeRepository.count() + 1).toString(),
+            name = name,
+            dateOfBirth = dateOfBirth,
+            city = city,
+            salary = salary,
+            gender = gender,
+            email = email
+        )
+        return employeeRepository.save(newEmployee)
+    }
 }
